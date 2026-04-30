7d46cb877a08dcb597f7fc2f8b28a15c7e4f9c7b
j-pma <jmaa0103@student.monash.edu>
2026-04-30 11:35:38 +1000

    export confirm

--- jp_code/export_csv.py
+++ jp_code/export_csv.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import os
 
 data = {'Name': ['Alice', 'Bob'], 'Age': [25, 30]}
 df = pd.DataFrame(data)
 
+filename = 'test.csv'
+
 df.to_csv('test.csv', index=False)
+print("Saved csv file to:", os.path.abspath(filename))
